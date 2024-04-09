bf0c64c4d76f82c23e88de624e0884bca44e941c
GrvExplorer <icodelife307@gmail.com>
2024-04-09 12:42:13 +0530

    ch 6

---
+++ app/_auth/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Authentication() {
+  return (
+    <div>Authentication Page</div>
+  )
+}
+
+export default Authentication
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import '@/app/ui/global.css'
 import { inter, lusitana } from './ui/fonts';
 
+export const metadata = {
+  title: 'Acme',
+  description: 'Acme landing page'
+} 
+
 
 export default function RootLayout({
   children,
@@ -9,6 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${lusitana.variable}`}>
+      <head>
+        <link rel='icon' href='/speedometer.png'></link>
+      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
